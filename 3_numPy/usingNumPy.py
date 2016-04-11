2f060b13922bc4952bd4d81d1006e6768384060b
liggettla <alexander.liggett@gmail.com>
2016-04-11 12:32:07 -0600

    examples using numpy

---
+++ 3_numPy/usingNumPy.py
@@ -0,0 +1,38 @@
+# numPy uses C to compute things and then passes to python
+# this makes numPy very fast
+
+import numpy as np
+
+def test_run():
+    # convert list to 1D array
+    np.array([2, 3, 4])
+    np.array([(2,3,4), (5,6,7)])
+
+# empty arrays actually initialize with random values dependent on current memory state
+    # 1D array
+    np.empty(5)
+    # 2D array
+    np.empty((5,4))
+    # 3D array
+    np.empty((5,4,3))
+    # 2D array with all values equal to 1
+    np.ones((5,4), dtype='i')
+    # default values are float, using dtype to change
+    np.array([1, 2, 3], dtype='i')
+    # generate random number array sampled from [0.0, 1.0]
+    np.random.random((5,4)) # pass in a tuple
+    np.random.rand(5,4) # not a tuple
+    # sample numbers from a gaussian distribution with mean=0 SD1=1
+    x = np.random.normal(loc=0, scale=1, size=(2,3))
+    np.random.normal
+    np.mean(x)
+    np.std(x)
+    # generate random integers
+    np.random.randint(10) # single integer between 0-10
+    np.random.randint(0, 10) # same as above
+    np.random.randint(0, 10, size=5) # integers between 0-10 filling 1D array
+    print np.random.randint(0, 10, size=(2, 4)) # 2x3 array of 0-10 integers
+
+
+if __name__ == "__main__":
+    test_run()
